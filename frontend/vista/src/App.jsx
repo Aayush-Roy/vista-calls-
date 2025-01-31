c9afa45b46b3d122b988a24d7206926c0679698e
aayush <ar0671362@gmail.com>
2025-01-31 13:09:35 +0530

    authentication testing

--- frontend/vista/src/App.jsx
+++ frontend/vista/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LandingPage from './pages/LandingPage';
 import Authentication from './pages/authentication';
+import { AuthProvider } from './contexts/AuthContext';
 
 
 
@@ -9,12 +10,15 @@ function App() {
   return (
     <div>
      <Router>
+      <AuthProvider>
+
       <Routes>
         {/* <Route path="/" element={<Home />} /> */}
         <Route path="/" element={<LandingPage/>} />
         <Route path="/auth" element={<Authentication />} />
         {/* <Route path="/meeting" element={<Meeting />} /> */}
       </Routes>
+      </AuthProvider>
      </Router>
     </div>
   )
